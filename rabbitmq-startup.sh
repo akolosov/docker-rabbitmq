83600cb286f99259e6cd979b0fc35035a3ef2e9f
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-11-17 11:47:04 +0500

    Update rabbitmq-startup.sh

--- rabbitmq-startup.sh
+++ rabbitmq-startup.sh
@@ -2,7 +2,11 @@
 
 MASTER_HOSTNAME="${MASTER_NAME}"
 
-MASTER_NODENAME="rabbit@master"
+if [ -z $RMQ_MASTER_HOSTNAME ]; then
+	MASTER_NODENAME="rabbit@master"
+else
+	MASTER_NODENAME="rabbit@$RMQ_MASTER_HOSTNAME"	
+fi
 
 ulimit -n 1024
 chown -R rabbitmq:rabbitmq /data
